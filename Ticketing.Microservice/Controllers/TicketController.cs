6847cb9a0a0a9719f16a73a7eaff2a79d166ad1e
Mukesh Murugan <iammukeshm@gmail.com>
2020-08-22 20:04:24 +0530

    Add project files.

---
+++ Ticketing.Microservice/Controllers/TicketController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Microservice.Models;
+
+namespace Ticketing.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            Uri uri = new Uri("rabbitmq://localhost/ticket-queue");
+            var endPoint = await _bus.GetSendEndpoint(uri);
+            await endPoint.Send(ticket);
+            return Ok();
+        }
+    }
+}
---
+++ Ticketing.Microservice/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ticketing.Microservice.Models
+{
+public class Ticket
+{
+    public string UserName { get; set; }
+    public DateTime BookedOn { get; set; } = DateTime.Now;
+    public string Boarding { get; set; }
+    public string Destination { get; set; }
+}
+}
